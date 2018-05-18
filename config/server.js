2712dd8dc9004305416c318335373816bd91dde0
lucasrfreire <lucas-rodrigues23@hotmail.com>
2018-05-18 18:38:28 -0300

    validando formulario de noticias

--- config/server.js
+++ config/server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var consign = require('consign');
 //tratativa de formularios
 var bodyParser = require('body-parser');
+//modulo que ajuda a fazer validações das requisições
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -12,6 +14,7 @@ app.set('views', './app/views');
 
 //adicionando como middleware, então já tem que informar como vai tratar os formularios
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 //aqui é definido as rotas com o consign, alem de incluir a configuração com o banco e as models
 consign()
