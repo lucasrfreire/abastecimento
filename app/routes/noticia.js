8b87c311aa36e7b8be7b4486168650e7c8a89ea4
lucasrfreire <lucas-rodrigues23@hotmail.com>
2018-05-18 18:48:35 -0300

    finalizando corretamente

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -7,4 +7,16 @@ module.exports = function (app) {
             res.render("noticias/noticia", {noticia : result});       
         });
     });
+
+
+    app.get('/noticias', function (req, res) {
+        //recuperando o modulo de conexão com o banco atraves do app
+        var connection = app.config.dbConnection();
+        var NoticiasDAO = new app.app.models.NoticiasDAO(connection);
+
+        NoticiasDAO.getNoticias(function (error, result) {
+            res.render("noticias/noticias", {noticias : result}); 
+            });
+       
+        });
 };
